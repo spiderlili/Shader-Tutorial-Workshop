57a1eb47164396ff56bd1d8539bd2b384eebbb78
Jing Tan <jtan1993@gmail.com>
2022-08-09 21:13:26 +0100

    Update TrigonometryFOV.cs

--- Assets/Scripts/TrigonometryFOV.cs
+++ Assets/Scripts/TrigonometryFOV.cs
@@ -1,18 +1,21 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
+// Make sure FOV encapsulates a dot. If you move this dot, update FOV accordingly to make sure it's always visible.
+// Because of the symmetry of the camera, having > 1 target that it should frame is just a matter of getting the maximum of them.
 public class TrigonometryFOV : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Transform objToBeFramed;
+    public Camera cam;
+    private void OnDrawGizmos()
     {
-        
-    }
+        if (cam == null) {
+            cam = GetComponent<Camera>();
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Vector2 relativeObjPos = objToBeFramed.position - cam.transform.position;
+
+        // Will only work if camera is at (0, 0)
+        float opposite = relativeObjPos.y;
+        float adjacent = relativeObjPos.x;
     }
 }
