68a6159b8070c6e1fa8b1da7bcc09784982a341f
Jing Tan <jtan1993@gmail.com>
2022-08-13 13:23:58 +0100

    add fov

--- Assets/Scripts/TrigonometryFOV.cs
+++ Assets/Scripts/TrigonometryFOV.cs
@@ -1,6 +1,6 @@
 ﻿using UnityEngine;
 
-// Make sure FOV encapsulates a dot. If you move this dot, update FOV accordingly to make sure it's always visible.
+// Make sure FOV encapsulates the objToBeFramed. If you move the obj: update FOV accordingly to make sure it's always visible.
 // Because of the symmetry of the camera, having > 1 target that it should frame is just a matter of getting the maximum of them.
 public class TrigonometryFOV : MonoBehaviour
 {
@@ -17,5 +17,10 @@ public class TrigonometryFOV : MonoBehaviour
         // Will only work if camera is at (0, 0)
         float opposite = relativeObjPos.y;
         float adjacent = relativeObjPos.x;
+        
+        // Tan(angle) = opp / adj 
+        // Angle = atan(opp / adj) 
+        float angleInRadians = Mathf.Atan(opposite / adjacent);
+        cam.fieldOfView = 2 * angleInRadians * Mathf.Rad2Deg;
     }
 }
